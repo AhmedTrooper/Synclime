108db41b6bfb966cba5d0f6e1cea326dc9c04e26
AhmedTrooper <151874789+AhmedTrooper@users.noreply.github.com>
2026-05-28 22:24:53 +0600

    added site ts demo

--- src/core/types/database.types.ts
+++ src/core/types/database.types.ts
@@ -4,3 +4,12 @@ export interface UserConfig {
   theme: "dark" | "light";
   downloadPath: string;
 }
+
+export interface SiteConfig {
+  slug: string;
+  title: string;
+  domain: string;
+  cookie_path: string;
+  is_default: 0 | 1;
+  created_at: string;
+}
